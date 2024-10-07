2cd28af9f7cf09bf7dab6b9e058248b01262e956
Sierra <smjanson@ucsc.edu>
2024-10-07 19:52:31 +0000

    30/30

--- asgn0/hello.c
+++ asgn0/hello.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
-int main(int argc, char **argv)
-{
-	(void) argc;
-	(void) argv;
-	fprintf(stdout, "Hello World!");
-	return 0;
+int main(int argc, char **argv) {
+    (void) argc;
+    (void) argv;
+    fprintf(stdout, "Hello World!");
+    return 0;
 }
