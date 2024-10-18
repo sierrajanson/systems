b3dd522820db5f939253b62bce610c4837d8a7d1
Sierra Janson <smjanson@ucsc.edu>
2024-10-18 16:03:54 +0000

    saving 75/100

--- asgn1/memory.c
+++ asgn1/memory.c
@@ -13,37 +13,37 @@ int fatal_error(const char *message) {
     exit(1);
 }
 
-int read_value(int max, char *value){
-	char c;
-	int count = 0;
-	int user_success = 0;
-	while (count < max && read(STDIN_FILENO, &c, 1) > 0) {
-	    	//printf("%c\n",c);
-		if (c == '\n') {
-		user_success = 1;
-		break;
-		}
-	    value[count] = c;
-	    count+=1;
-	}
-	if (user_success == 0) fatal_error("Invalid Command\n");    
-	return count;    
+int read_value(int max, char *value) {
+    char c;
+    int count = 0;
+    int user_success = 0;
+    while (count < max && read(STDIN_FILENO, &c, 1) > 0) {
+        //printf("%c\n",c);
+        if (c == '\n') {
+            user_success = 1;
+            break;
+        }
+        value[count] = c;
+        count += 1;
+    }
+    if (user_success == 0)
+        fatal_error("Invalid Command\n");
+    return count;
 }
-
-
 
 int main(int argc, char **argv) {
     char user_input[4];
-    read_value(4,user_input); 
+    read_value(4, user_input);
     //printf("command:%s\n",user_input);
-    if (strncmp(user_input, "set", 3) == 0) { // set --> set\n<location>\n<content_length>\n<contents>
+    if (strncmp(user_input, "set", 3)
+        == 0) { // set --> set\n<location>\n<content_length>\n<contents>
         // initialize FILENAME
         char filename[PATH_MAX]; // dynamically allocate
-        
-	int res = read_value(PATH_MAX, filename);
-	assert(res<4096);
-	filename[res]='\0';
-	//printf("got to filename\n");
+
+        int res = read_value(PATH_MAX, filename);
+        assert(res < 4096);
+        filename[res] = '\0';
+        //printf("got to filename\n");
         // check for valid text file
         char *txt = strstr(filename, ".txt");
         if (txt == NULL)
@@ -53,66 +53,77 @@ int main(int argc, char **argv) {
         int fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRWXU | S_IRGRP);
         if (fd == -1)
             fatal_error("Invalid Command\n");
-	//printf("opened file\n");
+        //printf("opened file\n");
         // initialize FILELENGTH
-	int maxlen = 12; // max length of string length
+        int maxlen = 12; // max length of string length
         char str_length[maxlen]; // str(len)
         char buffer[4096];
-	read_value(maxlen,str_length);
-	int length = atoi(str_length);
-	
-	if (length >= MAX){
+        read_value(maxlen, str_length);
+        int length = atoi(str_length);
+
+        /*if (length >= MAX){
 		printf("MAX MEMORY USING UNDEFIED BEHAVIOR\n");
 		exit(0);
-	}
-	// if no bytes are to be written
-	if (length == 0) { write(STDOUT_FILENO, "OK\n", 3);close(fd);exit(0);}
-	// reset tracker variables
-	//printf("past:%d\n",length);
-	char c;
-	int count = 0;
-	int already_written = 0;
-	// ---------------------------------------------------------------//
+	}*/
+        // if no bytes are to be written
+        if (length == 0) {
+            write(STDOUT_FILENO, "OK\n", 3);
+            close(fd);
+            exit(0);
+        }
+
+        // reset tracker variables
+        //printf("past:%d\n",length);
+        char c;
+        int count = 0;
+        int already_written = 0;
+        // ---------------------------------------------------------------//
         // READING IN MESSAGE CONTENT ------------------------------------//
-	// ---------------------------------------------------------------// 
+        // ---------------------------------------------------------------//
         // CHANGE TO 10 MB
-	while (count < length && read(STDIN_FILENO, &c, 1) > 0) {
-            if (count!=0 &&(count % 4096) == 0){
-		// must reset buffer, and write all characters to memory 
-		int bytes = 0;
-		while (bytes < 4096){
-			int val = write(fd,buffer,4096-bytes); //is this resetting the buffer, or do i need to update it
-			if (val < 0) fatal_error("Operation Failed\n");
-			bytes +=val;
-		}	
-		already_written+=4096;
-		for (int i = 0; i < 4096; i++){
-			buffer[i] = 'A';
-		}
-	    }
-	    if (c == '\n') {
-		//user_success = 1;
-            	buffer[count] = c;
-            	count++;
-		break;
+        if (length > 10000000)
+            length = 10000000;
+        while (count < length && read(STDIN_FILENO, &c, 1) > 0) {
+            if (count != 0 && (count % 4096) == 0) {
+                // must reset buffer, and write all characters to memory
+                int bytes = 0;
+                while (bytes < 4096) {
+                    int val = write(fd, buffer,
+                        4096 - bytes); //is this resetting the buffer, or do i need to update it
+                    // printf("bytes written:%d\n",val);
+                    if (val < 0)
+                        fatal_error("Operation Failed\n");
+                    bytes += val;
+                }
+                already_written += 4096;
+                for (int i = 0; i < 4096; i++) {
+                    buffer[i] = 'A';
+                }
+            }
+            if (c == '\n') {
+                //user_success = 1;
+                buffer[count % 4096] = c;
+                count++;
+                if (read(STDIN_FILENO, &c, 1) <= 0) {
+                    break;
+                }
                 // check if there's stuff after
             }
-            buffer[count] = c;
+            buffer[count % 4096] = c;
             count++;
         }
-        // append null terminator??
-
-        int msg_len = 0;
-	msg_len = strlen(buffer);
-        // partial write
-        if (length < msg_len) {
-            // may need to append null terminator
-        } else if (length > msg_len) { // not enough of msg to write
-            length = msg_len; // shorten length
-        } else { // ideal case where they are equal
+        //for (int i = 0; i < count; i++){
+        //	printf("%c",buffer[i]);
+        //}
+        //exit(0);
+        //printf("%d: total length\n",length);
+        //printf("%d: count\n",count);
+        //printf("%d:length of buffer\n", msg_len);
+        if (length > count) { // not enough of msg to write
+            length = count; // shorten length
         }
         int bytes = 0;
-	int goal = length - already_written;
+        int goal = length - already_written;
         while (bytes < goal) {
             bytes += write(fd, buffer, goal - bytes);
         }
@@ -135,23 +146,24 @@ int main(int argc, char **argv) {
         //        --> if filename is termiated by \n but there is still following
         char filename[PATH_MAX];
         int res = read_value(PATH_MAX, filename);
-	assert(res<4096);
-	filename[res]='\0';
-        
-	//printf("%s\n",filename);
-	char c; // may check for extra stuff after \n
-	//printf("acquired filename\n");
-	if (read(STDIN_FILENO, &c, 1) > 0) fatal_error("Invalid Command\n");
-	// no \n, or filename too long
-	//printf("no more stuff after file\n");
-	char *dirname=NULL;
-	if (opendir(filename) != NULL) {
-		free(dirname);
-		fatal_error("Invalid Command\n");
-	}
-	free(dirname);
-	//printf("filename:%s\n",filename);
-	int fd = open(filename, O_RDONLY, 0);
+        assert(res < 4096);
+        filename[res] = '\0';
+
+        //printf("%s\n",filename);
+        char c; // may check for extra stuff after \n
+        //printf("acquired filename\n");
+        if (read(STDIN_FILENO, &c, 1) > 0)
+            fatal_error("Invalid Command\n");
+        // no \n, or filename too long
+        //printf("no more stuff after file\n");
+        char *dirname = NULL;
+        if (opendir(filename) != NULL) {
+            free(dirname);
+            fatal_error("Invalid Command\n");
+        }
+        free(dirname);
+        //printf("filename:%s\n",filename);
+        int fd = open(filename, O_RDONLY, 0);
 
         // may not exceed 10,000,000 bytes
         if (fd == -1)
