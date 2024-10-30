1fdb78ae594d1b067e34ef41c52ea4fd11098aaf
Sierra Janson <smjanson@ucsc.edu>
2024-10-30 00:19:29 +0000

    updating

--- asgn2/httpserver.c
+++ asgn2/httpserver.c
@@ -30,19 +30,106 @@
  *
  *  @return void
  */
+
+void send_response(int soc, int status_code){
+	char *buf;
+	switch (status_code){
+		case 200:
+		    buf = "HTTP/1.1 200 OK\r\nContent-Length: 3\r\n\r\nOK\n";
+		    break;
+		case 201:
+		    buf = "HTTP/1.1 201 Created\r\nContent-Length: 8\r\n\r\nCreated\n";
+		    break;
+		case 400:
+		    buf = "HTTP/1.1 400 Bad Request\r\nContent-Length: 12\r\n\r\nBad Request\n";
+		    break;
+		case 403:
+		    buf = "HTTP/1.1 403 Forbidden\r\nContent-Length: 9\r\n\r\nForbidden\n";
+		    break;
+		case 404:
+		    buf = "HTTP/1.1 404 Not Found\r\nContent-Length: 9\r\n\r\nNot Found\n";
+		    break;
+		case 500:
+		    buf = "HTTP/1.1 500 Internal Server Error\r\nContent-Length: 21\r\n\r\nInternal Server Error\n";
+		    break;
+		case 501:
+		    buf = "HTTP/1.1 501 Not Implemented\r\nContent-Length: 16\r\n\r\nNot Implemented\n";
+		    break;
+		case 505:
+		    buf = "HTTP/1.1 505 Version Not Supported\r\nContent-Length: 22\r\n\r\nVersion Not Supported\n";
+		    break;
+		}
+	int res = write_n_bytes(soc, buf, strlen(buf));//soc, buf, n);
+	if (res == -1) printf("erorr sending code\n");
+	}
+
+int get(int connfd, const char *filename){
+	// retrieve content from file into a buffer
+	// write_n_bytes() all of message to client
+	
+	FILE *f = fopen(filename, "r");
+	if (f == NULL) { // FILE NOT FOUND
+		send_response(connfd, 404);
+	} else {	
+		int fd = open(filename, O_RDONLY, 0);
+		if (fd == -1) return -1;
+		char buffer[4096];
+		ssize_t bytes_read = 0;
+		ssize_t res = 0;
+		while ((res = read(fd, buffer, 4096)) > 0){
+			bytes_read += res;	
+		}
+		res = write_n_bytes(connfd, buffer, bytes_read);
+		if (res == -1) return -1;
+		fclose(f);
+		send_response(connfd, 200);
+	}	
+	// if length of a file is 0 do something special
+	return 0;
+}
+
+	
+int put(int connfd, const char *filename, const char *message){
+	// produce response with status-code, message-body, and content=length	
+    	printf("filename:%s message:%s\n",filename,message);
+	
+	FILE *f = fopen(filename, "r");
+	int code = 0;
+	if (f == NULL) {code = 201;} else {code = 200;fclose(f);}	
+	printf("code: %d\n",code);
+	
+	int fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRWXU | S_IRGRP);
+	if (fd == -1) return -1;
+	printf("fd created");	
+	unsigned long bytes_written = 0;
+	while (bytes_written < strlen(message)){
+		ssize_t res = write(fd, message + bytes_written, strlen(message) - bytes_written);
+		if (res < 0) return -1;
+		bytes_written += res;
+	}
+	printf("code: %d\n",code);
+	send_response(connfd, code);
+	return 0;	
+}
+
+
+
 void handle_connection(int connfd) {
-    /* Your code here */
     char buffer[2049];
     ssize_t res = read_until(connfd, buffer, 2048, ""); 
     if (res == -1){
-    	close(connfd);
- 	return;
-	} // maybe don't exit with error
+	printf("error\n");
+        return; 
+    } // maybe don't exit with error
     buffer[res] = '\0';
-    printf("%s\n",buffer);	
+    //int result = put(connfd, "happy.txt", "new");
+    int result = get(connfd, "hoppy.txt");
+    if (result == -1) printf("Error\n");
+    if (result == 0) printf("Success\n");
     close(connfd);
-    return;
 }
+
+
 
 /** @brief Main function for the HTTP server.
  *
@@ -72,7 +159,7 @@ int main(int argc, char **argv) {
 
     while (1) {
         int connfd = listener_accept(&sock);
-        handle_connection(connfd);
+	handle_connection(connfd);
     }
 
     return EXIT_SUCCESS;
