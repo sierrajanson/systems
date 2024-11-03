8586f715d6bd8d1f5b51b5d80a38ba5793ba39bf
Sierra Janson <smjanson@ucsc.edu>
2024-11-03 06:01:17 +0000

    ok

--- asgn2/httpserver.c
+++ asgn2/httpserver.c
@@ -9,7 +9,9 @@
 #include "asgn2_helper_funcs.h"
 #include "debug.h"
 #include "protocol.h"
-
+#include <assert.h>
+#include <dirent.h>
+#include <limits.h>
 #include <arpa/inet.h>
 #include <err.h>
 #include <errno.h>
@@ -22,10 +24,10 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <regex.h>
-#define BUFFER_SIZE 4096
-#define LENGTH_REGEX "([0-9]{1,128})"
-#define END_REGEX "\r\n(*{100})" // change later!!!
-#define EXTEND_REGEX HEADER_FIELD_REGEX END_REGEX
+#define BUFFER_SIZE  4096
+#define LENGTH_REGEX "([0-9]+{1,128})"
+//#define END_REGEX "\r\n(*{100})" // change later!!!
+//#define EXTEND_REGEX HEADER_FIELD_REGEX END_REGEX
 
 /** @brief Handles a connection from a client.
  *
@@ -34,158 +36,207 @@
  *  @return void
  */
 
-void send_response(int soc, int status_code){
-	char *buf;
-	switch (status_code){
-		case 200:
-		    buf = "HTTP/1.1 200 OK\r\nContent-Length: 3\r\n\r\nOK\n";
-		    break;
-		case 201:
-		    buf = "HTTP/1.1 201 Created\r\nContent-Length: 8\r\n\r\nCreated\n";
-		    break;
-		case 400:
-		    buf = "HTTP/1.1 400 Bad Request\r\nContent-Length: 12\r\n\r\nBad Request\n";
-		    break;
-		case 403:
-		    buf = "HTTP/1.1 403 Forbidden\r\nContent-Length: 9\r\n\r\nForbidden\n";
-		    break;
-		case 404:
-		    buf = "HTTP/1.1 404 Not Found\r\nContent-Length: 9\r\n\r\nNot Found\n";
-		    break;
-		case 500:
-		    buf = "HTTP/1.1 500 Internal Server Error\r\nContent-Length: 21\r\n\r\nInternal Server Error\n";
-		    break;
-		case 501:
-		    buf = "HTTP/1.1 501 Not Implemented\r\nContent-Length: 16\r\n\r\nNot Implemented\n";
-		    break;
-		case 505:
-		    buf = "HTTP/1.1 505 Version Not Supported\r\nContent-Length: 22\r\n\r\nVersion Not Supported\n";
-		    break;
-		}
-	int res = write_n_bytes(soc, buf, strlen(buf));//soc, buf, n);
-	if (res == -1) printf("erorr sending code\n");
-	}
+void send_response(int soc, int status_code) {
+    char *buf = NULL;
+    switch (status_code) {
+    case 200: buf = "HTTP/1.1 200 OK\r\nContent-Length: 3\r\n\r\nOK\n"; break;
+    case 201: buf = "HTTP/1.1 201 Created\r\nContent-Length: 8\r\n\r\nCreated\n"; break;
+    case 400: buf = "HTTP/1.1 400 Bad Request\r\nContent-Length: 12\r\n\r\nBad Request\n"; break;
+    case 403: buf = "HTTP/1.1 403 Forbidden\r\nContent-Length: 9\r\n\r\nForbidden\n"; break;
+    case 404: buf = "HTTP/1.1 404 Not Found\r\nContent-Length: 9\r\n\r\nNot Found\n"; break;
+    case 500:
+        buf = "HTTP/1.1 500 Internal Server Error\r\nContent-Length: 21\r\n\r\nInternal Server "
+              "Error\n";
+        break;
+    case 501:
+        buf = "HTTP/1.1 501 Not Implemented\r\nContent-Length: 16\r\n\r\nNot Implemented\n";
+        break;
+        "Supported\n";
+        break;
+    }
 
-int get(int connfd, const char *filename){
-	// retrieve content from file into a buffer
-	// write_n_bytes() all of message to client
-	
-	FILE *f = fopen(filename, "r");
-	if (f == NULL) { // FILE NOT FOUND
-		send_response(connfd, 404);
-	} else {	
-		int fd = open(filename, O_RDONLY, 0);
-		if (fd == -1) return -1;
-		char buffer[4096];
-		ssize_t bytes_read = 0;
-		ssize_t res = 0;
-		while ((res = read(fd, buffer, 4096)) > 0){
-			bytes_read += res;	
-		}
-		res = write_n_bytes(connfd, buffer, bytes_read);
-		if (res == -1) return -1;
-		fclose(f);
-		send_response(connfd, 200);
-	}	
-	// if length of a file is 0 do something special
-	return 0;
+    if (buf) {
+        if (write_n_bytes(soc, buf, strlen(buf)) == -1) {
+            fprintf(stderr, "Error sending response code %d\n", status_code);
+        }
+    }
+}
+int integer_len(int val) {
+    int count = 0;
+    while (val / 10 >= 1) {
+        count += 1;
+        val = val / 10;
+    }
+    return count;
 }
 
-	
-int put(int connfd, const char *filename, const char *message){
-	// produce response with status-code, message-body, and content=length	
-    	printf("filename:%s message:%s\n",filename,message);
-	
-	FILE *f = fopen(filename, "r");
-	int code = 0;
-	if (f == NULL) {code = 201;} else {code = 200;fclose(f);}	
-	printf("code: %d\n",code);
-	
-	int fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRWXU | S_IRGRP);
-	if (fd == -1) return -1;
-	printf("fd created");	
-	unsigned long bytes_written = 0;
-	while (bytes_written < strlen(message)){
-		ssize_t res = write(fd, message + bytes_written, strlen(message) - bytes_written);
-		if (res < 0) return -1;
-		bytes_written += res;
-	}
-	printf("code: %d\n",code);
-	send_response(connfd, code);
-	return 0;	
+int get(int connfd, const char *filename) {
+    char *dirname = NULL;
+    if (opendir(filename) != NULL) {
+        free(dirname);
+        send_response(connfd, 403);
+        return -1;
+    }
+    free(dirname);
+    int fd = open(filename, O_RDONLY, 0);
+
+    if (fd == -1) {
+        send_response(connfd, 404);
+        return -1;
+    }
+
+    struct stat fileStat;
+    if (stat(filename, &fileStat) != 0) {
+        send_response(connfd, 500);
+        return -1; // Error occurred
+    }
+    long long length = fileStat.st_size;
+    char *beginning = "HTTP/1.1 200 OK\r\nContent-Length: \r\n\r\n";
+    int size = strlen(beginning) + integer_len(length);
+    char response[size];
+
+    snprintf(response, size, "HTTP/1.1 200 OK\r\nContent-Length: %lld\r\n\r\n", length);
+
+    int res = write_n_bytes(connfd, response, size);
+    if (res == -1) {
+        send_response(connfd, 500);
+        return 1; // Error occurred
+    }
+
+    char buffer[PATH_MAX];
+    ssize_t bytes_read = 0;
+    while ((bytes_read = read(fd, buffer, PATH_MAX)) > 0) {
+        ssize_t bytes_written = 0;
+        // writing the bytes
+        while (bytes_written < bytes_read) {
+            ssize_t result = write(connfd, buffer + bytes_written, bytes_read - bytes_written);
+            // operation failed
+            if (result < 0) {
+                send_response(connfd, 500);
+                return 1;
+            }
+            bytes_written += result;
+        }
+    }
+    close(fd);
+    return 0;
 }
 
+int put(int connfd, const char *filename, const char *message) {
+    // produce response with status-code, message-body, and content=length
+    //printf("filename:%s message:%s\n", filename, message);
+    if (!filename || !message) {
+        fprintf(stderr, "Filename or message is NULL\n");
+        return -1;
+    }
+    FILE *f = fopen(filename, "r");
+    int code = 0;
+    if (f == NULL) {
+        code = 201;
+    } else {
+        code = 200;
+        fclose(f);
+    }
 
-void match_pattern(const char *buffer, char *substring, const char *pattern,int size){
-    // check for patterns
+    int fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRWXU | S_IRGRP);
+    if (fd == -1) {
+        send_response(connfd, 500);
+        return -1;
+    }
+    unsigned long bytes_written = 0;
+    while (bytes_written < strlen(message)) {
+        ssize_t res = write(fd, message + bytes_written, strlen(message) - bytes_written);
+        if (res < 0) {
+            send_response(connfd, 500);
+            return -1;
+        }
+        bytes_written += res;
+    }
+    if (close(fd) == -1) {
+        send_response(connfd, 500);
+        return -1;
+    }
+}
+
+send_response(connfd, code);
+return 0;
+}
+
+regoff_t match_pattern(const char *buffer, char *substring, const char *pattern, int size) {
     regex_t preg;
-    int rc = 0;
+    int rc = regcomp(&preg, pattern, REG_EXTENDED);
+    if (rc != 0) {
+        send_response(connfd, 500);
+        return -1;
+    }
+
     size_t nmatch = 8;
     regmatch_t pmatch[8];
-    regoff_t len;
-    const char *s = buffer;
-    	
-    if (0 != (rc = regcomp(&preg,pattern,REG_EXTENDED))){
-	printf("Failure\n");
-    }	    
-    if (0 != (rc = regexec(&preg, s, nmatch, pmatch, 0))) {
-   	printf("Failed to match '%s' with '%s',returning %d.\n",buffer, METHOD_REGEX, rc);
+    rc = regexec(&preg, buffer, nmatch, pmatch, 0);
+
+    if (rc != 0) {
+        // No match found, so we should free the regex and return -1
+        send_response(connfd, 500);
+        regfree(&preg);
+        return -1;
     }
-    len = pmatch[0].rm_eo - pmatch[0].rm_so;
-    
-    printf("substring = \"%.*s\"\n",len,s+pmatch[0].rm_so);	
-    snprintf(substring, size,"%.*s",len, s+pmatch[0].rm_so);
-	// return NULL upon failure, substring upon success
+
+    // Calculate the length of the matched substring
+    regoff_t len = pmatch[0].rm_eo - pmatch[0].rm_so;
+    if (len > size - 1) {
+        len = size - 1; // Ensure we don't exceed the buffer
+    }
+
+    snprintf(substring, size, "%.*s", (int) len, buffer + pmatch[0].rm_so);
+    regfree(&preg);
+    return pmatch[0].rm_eo; // Return the end offset of the match
 }
 
 void handle_connection(int connfd) {
     char buffer[2049];
-    ssize_t res = read_until(connfd, buffer, 2048, ""); 
-    if (res == -1){
-	printf("error\n");
-        return; 
-    } // maybe don't exit with error
+    ssize_t res = read_until(connfd, buffer, 2048, "");
+    if (res == -1) {
+        printf("error\n");
+        return;
+    }
     buffer[res] = '\0';
-    
-    // check for big format above
-  
+    //printf("%s",buffer);
+    //printf("lenght of buffer: %zd\n", strlen(buffer));
     char command[8];
-    match_pattern(buffer,command,METHOD_REGEX,8);
-    printf("COMMAND: %s\n",command);
-    // must do error handling
-    // if the command is GET or PUT there are different protocols
+    match_pattern(buffer, command, METHOD_REGEX, 8);
+    //printf("COMMAND: %s\n", command);
     char uri[64];
     match_pattern(buffer, uri, URI_REGEX, 64);
-    printf("URI: %s\n", uri);  
-    
-    if (strncmp(command, "GET", 3) == 0){
-    } else if (strncmp(command, "PUT", 3) == 0){
-	char holder[258];	
-        match_pattern(buffer, holder, HEADER_FIELD_REGEX, 128);
-        printf("HOLDeR: %s\n",holder); 
-	char value[128];
-        match_pattern(holder, value, LENGTH_REGEX, 128);
-	int length = atoi(value);
-	printf("MESSAGE LEN: %d\n",length);
+    //printf("URI: %s\n", uri);
+    // edge cases --> what if message is equal to max length
 
-	char holder2[1000];
-	match_pattern(buffer, holder2, EXTEND_REGEX, 1000);	
-	char message[length];
-        printf("--------------------------\n");
-        match_pattern(buffer, message, END_REGEX, length+4);
-        printf("--------------------------\n");
-	printf("MESSAGE:%s\n",message);
+    if (strncmp(command, "GET", 3) == 0) {
+        get(connfd, uri + 1);
+    } else if (strncmp(command, "PUT", 3) == 0) {
+        char holder[258];
+        int p = match_pattern(buffer, holder, HEADER_FIELD_REGEX, 128);
+        //printf("HOLDeR: %s\n", holder);
+        char value[128];
+        match_pattern(holder, value, LENGTH_REGEX, 128);
+        int length = atoi(value);
+        //printf("MESSAGE LEN: %d\n", length);
+        int message_pointer = p + 2;
+        char message[length + 1];
+        if (length > res - message_pointer) { // Check if `length` exceeds buffer limit
+            fprintf(stderr, "Message length exceeds buffer limit\n");
+            close(connfd);
+            return;
+        }
+        for (int i = message_pointer; i < message_pointer + length; i++) {
+            message[i - message_pointer] = buffer[i];
+        }
+        message[length] = '\0'; // terminate just in case
+        put(connfd, uri + 1, message); // add one to uri to bypass /
 
     } else {
-	printf("Command did not match either GET or PUT\n.");
+        send_response(connfd, 505);
+        return;
     }
-
-    
-    //int result = put(connfd, "happy.txt", "new");
-    //int result = get(connfd, "hoppy.txt");
-    //if (result == -1) printf("Error\n");
-    //if (result == 0) printf("Success\n");
-    close(connfd);
 }
 
 /** @brief Main function for the HTTP server.
@@ -209,14 +260,15 @@ int main(int argc, char **argv) {
     signal(SIGPIPE, SIG_IGN);
     Listener_Socket sock;
     int res = listener_init(&sock, port);
-    if (res < 0){
-	write(STDERR_FILENO, "Invalid port\n", 14); // ensure 14 not 13
-	return -1;
+    if (res < 0) {
+        write(STDERR_FILENO, "Invalid port\n", 14); // ensure 14 not 13
+        return -1;
     }
 
     while (1) {
         int connfd = listener_accept(&sock);
-	handle_connection(connfd);
+        handle_connection(connfd);
+        close(connfd);
     }
 
     return EXIT_SUCCESS;
