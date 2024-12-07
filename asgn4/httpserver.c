7b04071d6dbffc8d1eb05e4c5324916ffcf78769
Sierra <smjanson@ucsc.edu>
2024-12-07 00:59:09 +0000

    saving ig

--- asgn4/httpserver.c
+++ asgn4/httpserver.c
@@ -21,26 +21,35 @@
 #include "queue.h"
 #include "rwlock.h"
 #include <pthread.h>
+#include <search.h>
 #define OPTI "t"
 
+pthread_mutex_t lock;
+ENTRY item, *ret;
 struct thread_args {
-    rwlock_t *rw;
+    int num_threads;
+    int tid;
     queue_t *q;
 };
-void handle_connection(int, rwlock_t *rw);
-//void handle_connection(int);
-void handle_get(conn_t *);
-void handle_put(conn_t *);
-void handle_unsupported(conn_t *);
-void *thread_work_func(void *args) {
-    struct thread_args *t_args = (struct thread_args *) args;
-    rwlock_t *rw = t_args->rw;
-    queue_t *q = t_args->q;
 
+void handle_connection(int, int, int);//rwlock_t *rw);
+void handle_get(conn_t *,int);
+void handle_put(conn_t *,int);
+void handle_unsupported(conn_t *);
+
+// FUNCTION FOR THE THREAD TO COMPLETE
+void *thread_work_func(void *args) {
+	// PARSE ARGS
+	struct thread_args *t_args = (struct thread_args *) args;
+	int num_threads = t_args->num_threads;
+	queue_t *q = t_args->q;
+	int tid = t_args->tid;
+
+    // CONTINUOUSLY POP FROM QUEUE	
     while (1) {
         int *connfd;
         queue_pop(q, (void **) &connfd);
-        handle_connection(*connfd, rw);
+        handle_connection(*connfd, num_threads, tid);
         close(*connfd);
     }
     return NULL;
@@ -64,7 +73,7 @@ int main(int argc, char **argv) {
         case '?': printf("unknown option :%c\n", optopt); break;
         }
     }
-    assert(num_threads != 0 && "number of threads is 0, cannot continue");
+    assert(num_threads != 0);// ||  "number of threads is 0, cannot continue");
     char *endptr = NULL;
     size_t port = (size_t) strtoull(argv[optind + 1], &endptr, 10);
     if (endptr && *endptr != '\0') {
@@ -83,55 +92,70 @@ int main(int argc, char **argv) {
         fprintf(stderr, "Invalid Port\n");
         return EXIT_FAILURE;
     }
-
-    // initialize queue and RW lock
+	
+    // -----------------------------------
+    // CREATING THREADS
+    // ----------------------------------
     queue_t *q = queue_new(512);
-    rwlock_t *rw = rwlock_new(READERS, num_threads);
-
     struct thread_args args;
     args.q = q;
-    args.rw = rw;
+    args.num_threads = num_threads;
 
     pthread_t threads[num_threads];
     for (int i = 0; i < num_threads; i++) {
+	args.tid = i;    
         pthread_create(&threads[i], NULL, thread_work_func, (void *) &args);
     }
 
-    // initialize Queue if threads are blocked keep track of more requests
-    /* Initialize worker threads, the queue, and other data structures */
-    // block if all of your threads are active
-    /* Hint: You will need to change how handle_connection() is used */
+    // CREATING HASHMAP
+    hcreate(512);
+    
     while (1) {
         printf("waiting for connection...\n");
         int connfd = listener_accept(&sock);
         printf("connfd: %d\n", connfd);
         queue_push(q, (void *) &connfd);
     }
-    //handle_connection(connfd);
-    //close(connfd);
-    // if not possible --> wait
-    // continue loop
+
+    hdestroy();
     return EXIT_SUCCESS;
 }
 
-void handle_connection(int connfd, rwlock_t *rw) {
+void handle_connection(int connfd, int num_threads, int tid){ //, rwlock_t *rw) {
+    ENTRY holder, *found_holder;
     conn_t *conn = conn_new(connfd);
-
     const Response_t *res = conn_parse(conn);
-    // block in here if atomicity/concurrency is violated
+    
     if (res != NULL) {
         conn_send_response(conn, res);
     } else {
-        //debug("%s", conn_str(conn));
-        const Request_t *req = conn_get_request(conn);
-        if (req == &REQUEST_GET) {
-            reader_lock(rw);
-            handle_get(conn);
-            reader_unlock(rw);
+        // GET REQUEST
+	const Request_t *req = conn_get_request(conn);
+
+	// -------------------------------------
+	// PROTECT RW LOCK IN HASHMAP
+	// ------------------------------------
+        // trunc tmpfile, dont have to trunc uri
+	char *uri = conn_get_uri(conn);
+	holder.key = uri;
+	pthread_mutex_lock(&lock); // DS protected by a lock!
+    	found_holder = hsearch(holder, FIND);
+	printf("stuck in here?\n");
+	if (found_holder == NULL) {
+		printf("value was not in hashmpa\n");
+		holder.key = uri;
+		holder.data = rwlock_new(N_WAY, num_threads);
+		hsearch(holder, ENTER);
+   	 }else {
+	 	printf("value was in hashmap\n");
+	 }
+	pthread_mutex_unlock(&lock);
+	if (req == &REQUEST_GET) {
+	    printf("get \n");
+	    handle_get(conn, tid);
         } else if (req == &REQUEST_PUT) {
-            writer_lock(rw);
-            handle_put(conn);
-            writer_unlock(rw);
+	    printf("put \n");
+            handle_put(conn, tid);
         } else { // do we need a lock for this?
             handle_unsupported(conn);
         }
@@ -139,22 +163,29 @@ void handle_connection(int connfd, rwlock_t *rw) {
     conn_delete(&conn);
 }
 
-void handle_get(conn_t *conn) {
+void handle_get(conn_t *conn, int tid) {
+    // SEARCH FOR LOCK
+    (void) tid;
     char *uri = conn_get_uri(conn);
+    item.key = uri;
+    ret = hsearch(item, FIND);
+    rwlock_t *rw = (rwlock_t *) ret->data; 
     //debug("Handling GET request for %s", uri);
     printf("URI of file: %s\n", uri);
+
+    // GET RID
     char *rid = conn_get_header(conn, "Request-Id");
     printf("RID of file: %s\n", rid);
     int sc = 0;
-    // What are the steps in here?
-
-    // 1. Open the file.
-    // If open() returns < 0, then use the result appropriately
-    //   a. Cannot access -- use RESPONSE_FORBIDDEN
-    //   b. Cannot find the file -- use RESPONSE_NOT_FOUND
-    //   c. Other error? -- use RESPONSE_INTERNAL_SERVER_ERROR
-    // (Hint: Check errno for these cases)!
+    reader_lock(rw);
     int fd = open(uri, O_RDONLY);
+    struct stat file_stat;
+    if (fstat(fd, &file_stat) == -1) {
+        printf("ERROR reading length of file\n");
+    } else {
+	printf("ERROR reading length of file\n");
+    }
+    
     printf("fd: %d\n", fd);
     if (fd == -1) {
         printf("couldn't open file\n");
@@ -181,21 +212,6 @@ void handle_get(conn_t *conn) {
         return;
     }
 
-    struct stat file_stat;
-    if (fstat(fd, &file_stat) == -1) {
-        printf("ERROR reading length of file\n");
-    } else {
-        printf("got length of file successfully\n");
-    }
-    // 2. Get the size of the file.
-    // (Hint: Checkout the function fstat())!
-
-    // 3. Check if the file is a directory, because directories *will*
-    // open, but are not valid.
-    // (Hint: Checkout the macro "S_IFDIR", which you can use after you call fstat()!)
-
-    // 4. Send the file
-    // (Hint: Checkout the conn_send_file() function!)
     const Response_t *res = conn_send_file(conn, fd, file_stat.st_size);
     if (res != NULL) {
         printf("error in sending file!\n");
@@ -206,34 +222,51 @@ void handle_get(conn_t *conn) {
         printf("request id was null\n");
         rid = "0";
     }
-    fprintf(stderr, "GET,/%s,%d,%s\n", uri, sc, rid);
     close(fd);
-    // 5. Close the file
+    fprintf(stderr, "GET,/%s,%d,%s\n", uri, sc, rid);
+    reader_unlock(rw);
 }
-
-void handle_put(conn_t *conn) {
-    char *uri = conn_get_uri(conn);
+// tnam
     // debug("Handling PUT request for %s", uri);
+void handle_put(conn_t *conn, int tid){//, int num_threads) {
+   
+    // -----------------------------------   	
+    // FINDING RW LOCK
+    // -----------------------------------
+    char *uri = conn_get_uri(conn);
     char *rid = conn_get_header(conn, "Request-Id");
-    int sc = 0;
-    // What are the steps in here?
+    item.key = uri;
+    ret = hsearch(item, FIND);
+    rwlock_t *rw = (rwlock_t *)ret->data; 
+    
+    // ------------------------------------
+    // CREATING TMP FILENAME
+    char conn_to_str[10];
+    sprintf(conn_to_str, "%d", tid);
+    char tmp_fn[] = "ti";
+    strcat(tmp_fn, conn_to_str);
+    //char generate[L_tmpnam + 1];
+    //tmpnam(generate); 
+    // -----------------------------------
+   
+    // CREATE UNIQUE TMP FILE
+    int tmp_fd = open(tmp_fn, O_CREAT | O_WRONLY | O_TRUNC, S_IRWXU | S_IRGRP);
+    assert(tmp_fd != -1);
+    const Response_t *result = conn_recv_file(conn, tmp_fd);	
+    close(tmp_fd);
 
-    // 1. Check if file already exists before opening it.
-    // (Hint: check the access() function)!
+   // LOCKING --> ONCE FILE HAS BEEN WRITTEN 
+    writer_lock(rw);
+    
+    int sc = 0;
     if (access(uri, F_OK) != 0) { // have to create file if it doesn't exist
         sc = 201;
     } else {
         sc = 200;
     }
-    // 2. Open the file.
-    // If open() returns < 0, then use the result appropriately
-    //   a. Cannot access -- use RESPONSE_FORBIDDEN
-    //   b. File is a directory -- use RESPONSE_FORBIDDEN
-    //   c. Cannot find the file -- use RESPONSE_FORBIDDEN
-    //   d. Other error? -- use RESPONSE_INTERNAL_SERVER_ERROR
-    // (Hint: Check errno for these cases)!
 
     int fd = open(uri, O_CREAT | O_WRONLY | O_TRUNC, S_IRWXU | S_IRGRP);
+    rename(tmp_fn, uri);
     printf("file opened\n");
     if (fd == -1) {
         strerror(errno);
@@ -258,21 +291,13 @@ void handle_put(conn_t *conn) {
         close(fd);
         return;
     }
-    printf("got through to here\n");
-    // 3. Receive the file
-    // (Hint: Checkout the conn_recv_file() function)!
-    const Response_t *result = conn_recv_file(conn, fd);
-    printf("file received\n");
-    // 4. Send the response
-    // (Hint: Checkout the conn_send_response() function)!
     if (sc == 200)
-    result = conn_send_response(conn, &RESPONSE_OK);
+    	result = conn_send_response(conn, &RESPONSE_OK);
     if (sc == 201)
-    result = conn_send_response(conn, &RESPONSE_CREATED);
+    	result = conn_send_response(conn, &RESPONSE_CREATED);
     if (result != NULL) {
         printf("error in receiving file!!!\n");
     }
-    // 5. Close the file
     close(fd);
     printf("file closed\n");
     if (rid == NULL) {
@@ -280,12 +305,9 @@ void handle_put(conn_t *conn) {
         rid = "0";
     }
     fprintf(stderr, "PUT,/%s,%d,%s\n", uri, sc, rid);
-    printf("audit log wrirten too\n");
+    writer_unlock(rw);
 }
 
 void handle_unsupported(conn_t *conn) {
-    // debug("Handling unsupported request");
-
-    // Send responses
     conn_send_response(conn, &RESPONSE_NOT_IMPLEMENTED);
 }
